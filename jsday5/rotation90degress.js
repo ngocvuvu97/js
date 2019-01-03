9db22ed4d3ef7e274030e723a4855cd49dea3030
ngocvuvu97 <46150745+ngocvuvu97@users.noreply.github.com>
2019-01-03 13:19:20 +0700

    Update rotation90degress.js

--- jsday5/rotation90degress.js
+++ jsday5/rotation90degress.js
@@ -1,14 +1,13 @@
-
-function rotation(arr) {
+function rota(arr) {
      arr = arr.reverse();
      debugger;
      for (var i = 0; i < arr.length; i++) {
        for (var j = 0; j < i; j++) {
-        var temp = arr[i][j];
+        var backup = arr[i][j];
         arr[i][j] = arr[j][i];
-        arr[j][i] = temp;
+        arr[j][i] = backup;
        }
      }
      return arr;
    }
-   console.log(rotation([[1,2,3],[4,5,6],[7,8,9]]))
+   console.log(rota([[1,2,3],[4,5,6],[7,8,9]]))
