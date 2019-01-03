e2d50892bc1f00db04526b1c6ebe050ab6598160
ngocvuvu97 <46150745+ngocvuvu97@users.noreply.github.com>
2019-01-03 13:17:08 +0700

    Update bookHotel.js

--- jsday5/bookHotel.js
+++ jsday5/bookHotel.js
@@ -2,20 +2,22 @@
   function book(arrival, departure , room) {
     arrival.sort();
     departure .sort();
-    var i = 0, j =0 , avaiable=0;
-    while (i < arrival.length && j < departure .length) {
+    var i = j = current = 0;
+    day= arrival.length;
+    while (i < day && j < day) {
         if (arrival[i] < departure [j]) {
             i++;
-            avaiable++;
+            current++;
         }
         else {
             j++;
-            avaiable--;
+            current--;
         }
-        if (avaiable > room) {
+        if (current > room) {
             return false;
         }
     }
     return true;
 }
-console.log(book([1, 3, 5],[2, 5, 8],1))
+console.log(book([1, 3, 5],[2, 6, 8],1))
+console.log(book([1, 3, 5],[2, 4, 8],1))
