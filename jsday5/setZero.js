e8cdec7a0c454071a83256ee63e71ee02293cdd4
ngocvuvu97 <46150745+ngocvuvu97@users.noreply.github.com>
2019-01-03 13:18:15 +0700

    Update setZero.js

--- jsday5/setZero.js
+++ jsday5/setZero.js
@@ -1,37 +1,62 @@
-function matrix(arr){
-    var res = new Array();
-    for( i in arr){
-        res[i]=[];
+// function matrix(arr){
+//     var res = new Array();
+//     for( i in arr){
+//         res[i]=[];
+//         for(j in arr){
+//             res[i][j]=1;
+//         }
+//     }
+//     return res;
+// }
+// function setnum(arr, res){
+//     for( var i in arr){
+//         for ( j in arr) {
+//             if(arr[i][j]==0 ){
+//                 for(x in arr){
+//                     res[i][x]=0;
+//                 }
+//                 for(z in arr){
+//                     res[z][j]=0;
+//                 }
+//             }
+//         }
+//     }
+//     return res;
+// }
+// function set0(arr){
+//     var res=matrix(arr);
+//     return setnum(arr,res);
+// }
+
+
+
+function setzero(arr){
+    for ( i in arr){
         for(j in arr){
-            res[i][j]=1;
-        }
-    }
-    return res;
-}
-function setnum(arr, res){
-    for( var i in arr){
-        for ( j in arr) {
-            if(arr[i][j]==0 ){
-                for(x in arr){
-                    res[i][x]=0;
+            if(arr[i][j]==0){
+                for( z in arr){
+                    arr[i][z]==0?arr[i][z]=0:arr[i][z]=2
                 }
-                for(z in arr){
-                    res[z][j]=0;
+                for( k in arr){
+                    arr[k][j]==0?arr[k][j]=0:arr[k][j]=2
                 }
             }
         }
     }
-    return res;
+    for ( i in arr){
+        for(j in arr){
+            if(arr[i][j]==2){
+                arr[i][j] =0
+            }
+        }
+    }
+    return arr;
 }
-function set0(arr){
-    var res=matrix(arr);
-    return setnum(arr,res);
-}
-console.log(set0(
+console.log(setzero(
     [
         [1, 0, 1, 0],
         [1, 1, 1, 1],
         [1, 1, 1, 1],
         [0, 1, 1, 1]
         ]
-))
+));
