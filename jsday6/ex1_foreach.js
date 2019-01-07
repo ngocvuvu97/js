c713a86e9570012684bcbf6e1ddb79dc0691752e
ngocvuvu97 <46150745+ngocvuvu97@users.noreply.github.com>
2019-01-07 14:00:57 +0700

    Update ex1_foreach.js

--- jsday6/ex1_foreach.js
+++ jsday6/ex1_foreach.js
@@ -7,15 +7,46 @@ const data = JSON.parse(fs.readFileSync('./JavaScript Advanced 02 - Higher-Order
 //         res.push(`${detail[i].first_name} ${detail[i].last_name}`)
 //     }
 //     return res;
-//console.log(forEach(data))
+// console.log(forEach(data))
 
 //ex1
-const arr1 = []
+const arr1 = [];
 data.forEach((data) => {
-    let result = `${data.first_name+" "+ data.last_name}`;
-    arr1.push(result);
+    arr1.push(`${data.first_name+" "+ data.last_name}`)
 })
 //console.log(arr1)
 
 //ex2
+const arr2 = [];
+data.filter(function(data){
+    if(data.age < 40 && data.gender=="Male" ){
+        arr2.push(data)
+    }
+})
+//console.log(arr2);
+//ex3
+const arr3 = [];
+data.map(function(data) {
+    arr3.push(`${data.first_name+" "+ data.last_name}`)
+});
+//console.log(arr3);
+//ex4
+data.map(function (data) { 
+    data["firstName"]=data["first_name"];
+    data["lastName"]=data["last_name"];
+    delete data["first_name"];
+    delete data["last_name"];
+});
+//console.log(data);
+
+//ex5
+
+let sumage = (data.reduce((age, data) => {
+    return age += data.age;
+}, 0))/data.length
+//console.log(sumage);
+//ex
+
+
+
 
